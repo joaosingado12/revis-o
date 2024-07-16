1ce3dbf0767faac1191eae7d7c53fbfeca142d00
joaosingado12 <110484414+joaosingado12@users.noreply.github.com>
2024-07-16 14:01:05 -0300

    att

--- jogadores/models.py
+++ jogadores/models.py
@@ -10,6 +10,5 @@ class Jogadores(models.Model):
     time = models.CharField(max_length=100)
     posicao = models.CharField(max_length=20)
     foto = models.ImageField(upload_to='foto_jogadores', null=True, blank=True)
-
     def __str__(self):
         return self.nome
--- jogadores/urls.py
+++ jogadores/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import home, salvar, editar, update, excluir, novo
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('update/<int:id>', update, name='update'),
     path('excluir/<int:id>', excluir, name='excluir'),
     path('novo/', novo, name='novo')
-]
+]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
