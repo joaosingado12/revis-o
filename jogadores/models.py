c20acf5fd2e253892e203f0a82aca6c0fadb27ad
joaosingado12 <110484414+joaosingado12@users.noreply.github.com>
2024-07-25 17:24:02 -0300

    25/7

--- jogadores/models.py
+++ jogadores/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Clube(models.Model):
+    nome = models.CharField(max_length=50)
+    foto = models.ImageField(upload_to="foto_clubes", null=True, blank=True)
+    def __str__(self):
+        return self.nome
+
 class Jogadores(models.Model):
     nome = models.CharField(max_length=100)
     numero = models.IntegerField()
     idade = models.IntegerField()
     altura = models.DecimalField(max_digits=3, decimal_places=2)
-    time = models.CharField(max_length=100)
     posicao = models.CharField(max_length=20)
     foto = models.ImageField(upload_to='foto_jogadores', null=True, blank=True)
     descricao = models.CharField(max_length=300, null=True, blank=True)
+    clube = models.ForeignKey(Clube, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return self.nome
--- setup/settings.py
+++ setup/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "joga1",
+        'USER': "root",
+        'PASSWORD': "Senac123@",
+        'HOST': "localhost",
+        'PORT': "3306"
     }
 }
 
